82c5abcc891bfd9604276694f65d8a6eca5e4f95
kodyjsch <dakota.schweitzer04@gmail.com>
2026-01-16 16:35:47 -0600

    Airplane and Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,11 +15,17 @@ public class PlayerController1 : MonoBehaviour
     private float movementX;
     private float movementY;
 
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
         rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
 
         SetCountText();
         winTextObject.SetActive(false);
@@ -48,6 +54,7 @@ public class PlayerController1 : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -70,6 +77,22 @@ public class PlayerController1 : MonoBehaviour
             // Update the winText to display "You Lose!"
             winTextObject.gameObject.SetActive(true);
             winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
     }
 
