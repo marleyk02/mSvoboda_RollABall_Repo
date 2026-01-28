62f2154b9f2c7bbc4712e085f34edd56f41003ee
awignall3 <awignallop@gmail.com>
2026-01-28 16:22:20 -0600

    splatter effect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Events;
 using TMPro;
 
-public class PlayerController1 : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public UnityEvent onHit;
+    private Rigidbody rb;
+    private int count;
+
+    private float movementX;
+    private float movementY;
+
     public float speed = 0;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
 
-    private Rigidbody rb;
-    private int count;
-    private float movementX;
-    private float movementY;
+    // NEW: particle explosion prefab for the ball
+    public GameObject explosionEffect;
 
-    public Vector3 jump;
-    public float jumpForce = 2.0f;
-
-    public bool isGrounded;
-
-    // Start is called before the first frame update
     void Start()
     {
-        count = 0;
         rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
-
+        count = 0;
         SetCountText();
         winTextObject.SetActive(false);
     }
@@ -36,69 +28,54 @@ public class PlayerController1 : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            // Spawn explosion at the ball's position
+            if (explosionEffect != null)
+            {
+                Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            }
+
+            // Increase count
+            count++;
+            SetCountText();
+
+            // Destroy the ball
+            Destroy(gameObject);
+        }
     }
 
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+
+        if (count >= 12)
         {
             winTextObject.SetActive(true);
             Destroy(GameObject.FindGameObjectWithTag("Enemy"));
         }
     }
 
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-        rb.AddForce(movement * speed);
-
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("PickUp"))
-        {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText();
-
-            onHit.Invoke();
-            gameObject.SetActive(false);
-        }
-
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            // Destroy the current object
             Destroy(gameObject);
-            // Update the winText to display "You Lose!"
-            winTextObject.gameObject.SetActive(true);
+            winTextObject.SetActive(true);
             winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
         }
-
     }
-
-    void OnCollisionStay()
-    {
-        isGrounded = true;
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-
-            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }
-    }
-
 }
+
